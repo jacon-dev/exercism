03a30e6392ced199cb2ea99ec34c276ce08935a7
JC <jacon.dev@outlook.com>
2025-01-01 21:52:07 +0000

    Adding further tests

--- exercism/AnnalynsInfiltration/InfiltrationTests.cs
+++ exercism/AnnalynsInfiltration/InfiltrationTests.cs
@@ -232,15 +232,15 @@ namespace infiltration.tests
             Assert.False(Infiltration.CanFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent));
         }
 
-        //[Test]
-        //public void Cannot_free_prisoner_if_only_knight_is_asleep_and_pet_dog_is_absent()
-        //{
-        //    var knightIsAwake = false;
-        //    var archerIsAwake = true;
-        //    var prisonerIsAwake = true;
-        //    var petDogIsPresent = false;
-        //    Assert.False(Infiltration.CanFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent));
-        //}
+        [Test]
+        public void Cannot_free_prisoner_if_only_knight_is_asleep_and_pet_dog_is_absent()
+        {
+            var knightIsAwake = false;
+            var archerIsAwake = true;
+            var prisonerIsAwake = true;
+            var petDogIsPresent = false;
+            Assert.False(Infiltration.CanFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent));
+        }
 
         //[Test]
         //public void Can_free_prisoner_if_only_archer_is_asleep_and_pet_dog_is_present()
