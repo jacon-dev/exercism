3f9e117b4682e662d0642091ca7e08a1e320c1c0
JC <jacon.dev@outlook.com>
2025-01-01 22:23:58 +0000

    Initial refactorings

--- exercism/infiltration/Infiltration.cs
+++ exercism/infiltration/Infiltration.cs
@@ -2,35 +2,19 @@
 {
     public static class Infiltration
     {
-        public static bool CanFastAttack(bool knightIsAwake)
-        {
-            return !knightIsAwake;
-        }
+        public static bool CanFastAttack(bool knightIsAwake) => !knightIsAwake;
 
-        public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
-        {
-            return !new[] { knightIsAwake, archerIsAwake, prisonerIsAwake }.All(x => !x);
-        }
+        public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake) => !new[] { knightIsAwake, archerIsAwake, prisonerIsAwake }.All(x => !x);
 
-        public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
-        {
-            return (!archerIsAwake && prisonerIsAwake) ? true : false;
-        }
+        public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake) => !archerIsAwake && prisonerIsAwake;
 
         public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
         {
             if (new[] { knightIsAwake, archerIsAwake, prisonerIsAwake }.All(x => x)) return false;
-            if (new[] { knightIsAwake, archerIsAwake, prisonerIsAwake }.All(x => !x))
-            {
-                return petDogIsPresent;
-            }
+            if (new[] { knightIsAwake, archerIsAwake, prisonerIsAwake }.All(x => !x)) return petDogIsPresent;
             if (archerIsAwake) return false;
-            if (knightIsAwake)
-            {
-                return petDogIsPresent;
-            }
-            if (!prisonerIsAwake) return false;
-            return true;
+            if (knightIsAwake) return petDogIsPresent;
+            return prisonerIsAwake;
         }
     }
 }
