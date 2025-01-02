15765907e7db7e6d15520857fc0dabb3efa5ce3f
JC# <jacon.dev@outlook.com>
2025-01-02 20:07:42 +0000

    Final Refactoring
    Following submission another solution had used a much simpler set of checks. reviewed and replaced code accordingly to streamline

--- exercism/infiltration/Infiltration.cs
+++ exercism/infiltration/Infiltration.cs
@@ -8,13 +8,6 @@
 
         public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake) => !archerIsAwake && prisonerIsAwake;
 
-        public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
-        {
-            if (new[] { knightIsAwake, archerIsAwake, prisonerIsAwake }.All(x => x)) return false;
-            if (new[] { knightIsAwake, archerIsAwake, prisonerIsAwake }.All(x => !x)) return petDogIsPresent;
-            if (archerIsAwake) return false;
-            if (knightIsAwake) return petDogIsPresent;
-            return prisonerIsAwake;
-        }
+        public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent) => (petDogIsPresent && !archerIsAwake) || (!petDogIsPresent && prisonerIsAwake && !knightIsAwake && !archerIsAwake);
     }
 }
